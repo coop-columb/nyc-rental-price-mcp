be6dea1a722064f31def80f7502a09e3c1c5c836
coop-columb <ahc2188@columbia.edu>
2025-03-27 18:14:51 -0400

    Add initial project structure with preprocessing, modeling, and documentation

---
+++ model.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+def build_model(input_shape):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(64, activation='relu', input_shape=input_shape),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+    return model
+
