d1d434e10791d913fcfe5f2e010d9044d00efbe4
coop-columb <ahc2188@columbia.edu>
2025-03-31 03:22:34 -0400

    refactor: simplify __init__.py by removing unused imports and updating __all__ list

--- src/nyc_rental_price/__init__.py
+++ src/nyc_rental_price/__init__.py
@@ -1,20 +1,10 @@
-"""NYC Rental Price Prediction package.
+"""NYC Rental Price Prediction package."""
 
-This package provides functionality for predicting rental prices in New York City.
-"""
+# Import basic components
+from src.nyc_rental_price.models.model import (
+    GradientBoostingModel,
+    ModelEnsemble,
+    NeuralNetworkModel,
+)
 
-from src.nyc_rental_price.data import preprocessing
-from src.nyc_rental_price.features import FeaturePipeline
-from src.nyc_rental_price.models.model import GradientBoostingModel, NeuralNetworkModel, ModelEnsemble
-from src.nyc_rental_price.models.train import train_and_evaluate
-
-__version__ = "1.0.0"
-
-__all__ = [
-    "preprocessing",
-    "FeaturePipeline",
-    "GradientBoostingModel",
-    "NeuralNetworkModel",
-    "ModelEnsemble",
-    "train_and_evaluate",
-]
+__all__ = ["GradientBoostingModel", "NeuralNetworkModel", "ModelEnsemble"]
